adfb07fd04abdf97be3dc9f0fc5687b0d08fe54a
Gabriel Carvalho <87557540+aquelegabriel@users.noreply.github.com>
2021-08-28 10:53:58 -0300

    Add files via upload

---
+++ m4Lware.bat
@@ -0,0 +1,10 @@
+@echo off
+title TITULO
+cd/
+cd Users
+cd adail
+cd desktop
+md HACK %random%
+cd HACK
+echo > %random%.bat
+pause
---
+++ troj4n.bat
@@ -0,0 +1,43 @@
+@echo off
+systeminfo
+echo.
+echo INFECTED_RANSONWARE-VIRUSTYPE:YJG155.NETSHWLAN.func [public_ip_hidden];name=troj4n.bat?windows.defender=disabled_for_realtime_scan///INFECTED_RANSONWARE-VIRUSTYPE:YJG155.NETSHWLAN.func [public_ip_hidden];name=troj4n.bat?windows.defender=disabled_for_realtime_scan///INFECTED_RANSONWARE-VIRUSTYPE:YJG155.NETSHWLAN.func [public_ip_hidden];name=troj4n.bat?windows.defender=disabled_for_realtime_scan///INFECTED_RANSONWARE-VIRUSTYPE:YJG155.NETSHWLAN.func [public_ip_hidden];name=troj4n.bat?windows.defender=disabled_for_realtime_scan///INFECTED_RANSONWARE-VIRUSTYPE:YJG155.NETSHWLAN.func [public_ip_hidden];name=troj4n.bat?windows.defender=disabled_for_realtime_scan///INFECTED_RANSONWARE-VIRUSTYPE:YJG155.NETSHWLAN.func [public_ip_hidden];name=troj4n.bat?windows.defender=disabled_for_realtime_scan
+echo ON_RANSONWARE
+echo SYSTEM_CORRUPT-true; hdd-access-encrypt-data-ontime
+echo.
+ipconfig /all
+echo.
+echo SYSTEM_CORRUPT-activated;
+echo INFECTED_RANSONWARE-VIRUSTYPE:ACC872.NETSHWLAN.static [public_ip_hidden]; name=troj4n.bat
+echo MALWARE_TROJ4N.BAT=LIFEGUARD.FREETIME.ACCESS [main_entry_open]; name=troj4n.bat
+echo.
+driverquery
+echo.
+echo SYSTEM_COPY@my_FRAMEWORK.1880.dotnet
+echo.
+echo ***SYSTEM: "YOU GOT HACKED BY: MR ROBOT!"
+echo.
+echo >> aiqkrudjsow.tyl
+echo >> bjekflcmdoap.daj
+echo >> asudewr7r7e.uop
+echo >> nome1.tyl
+echo >> nome2.daj
+echo >> nome3.uop
+echo >> nome4.tyl
+echo >> 5.daj
+echo >> 6.uop
+echo >> 7.tyl
+echo >> 8.daj
+md 9.uop
+echo ***INFECTED_RANSONWARE-VIRUSTYPE:YJG155.NETSHWLAN.func [public_ip_hidden]; name=troj4n.bat///INFECTED_RANSONWARE-VIRUSTYPE:YJG155.NETSHWLAN.func [public_ip_hidden];name=troj4n.bat?windows.defender=disabled_for_realtime_scan///INFECTED_RANSONWARE-VIRUSTYPE:YJG155.NETSHWLAN.func [public_ip_hidden];name=troj4n.bat?windows.defender=disabled_for_realtime_scan///INFECTED_RANSONWARE-VIRUSTYPE:YJG155.NETSHWLAN.func [public_ip_hidden];name=troj4n.bat?windows.defender=disabled_for_realtime_scan///INFECTED_RANSONWARE-VIRUSTYPE:YJG155.NETSHWLAN.func [public_ip_hidden];name=troj4n.bat?windows.defender=disabled_for_realtime_scan///INFECTED_RANSONWARE-VIRUSTYPE:YJG155.NETSHWLAN.func [public_ip_hidden];name=troj4n.bat?windows.defender=disabled_for_realtime_scan
+echo.
+systeminfo
+echo.
+echo INFECTED_RANSONWARE-VIRUSTYPE:YJG155.NETSHWLAN.func [public_ip_hidden];name=troj4n.bat?windows.defender=disabled_for_realtime_scan///INFECTED_RANSONWARE-VIRUSTYPE:YJG155.NETSHWLAN.func [public_ip_hidden];name=troj4n.bat?windows.defender=disabled_for_realtime_scan///INFECTED_RANSONWARE-VIRUSTYPE:YJG155.NETSHWLAN.func [public_ip_hidden];name=troj4n.bat?windows.defender=disabled_for_realtime_scan///INFECTED_RANSONWARE-VIRUSTYPE:YJG155.NETSHWLAN.func [public_ip_hidden];name=troj4n.bat?windows.defender=disabled_for_realtime_scan///INFECTED_RANSONWARE-VIRUSTYPE:YJG155.NETSHWLAN.func [public_ip_hidden];name=troj4n.bat?windows.defender=disabled_for_realtime_scan///INFECTED_RANSONWARE-VIRUSTYPE:YJG155.NETSHWLAN.func [public_ip_hidden];name=troj4n.bat?windows.defender=disabled_for_realtime_scan
+echo ON_RANSONWARE
+echo SYSTEM_CORRUPT-true; hdd-access-encrypt-data-ontime
+echo.
+start explorer.exe
+start mspaint
+pause
+rem /* AQUI COMEÇA O "TASK KILL OU 'SHUTDOWN'" */
